11d6b9f2d350e5747087095839b4794d58b828d4
Oleksandr Lysak <lisak.aleksandr@gmail.com>
2024-07-10 19:30:03 +0200

    FizzBuzz HW12.5

--- src/Exercise2/FizzBuzz.java
+++ src/Exercise2/FizzBuzz.java
@@ -3,65 +3,56 @@ package Exercise2;
 class FizzBuzz {
     private final int n;
     private int current = 1;
-    private final Object lock = new Object();
 
     public FizzBuzz(int n) {
         this.n = n;
     }
 
-    public void fizz() throws InterruptedException {
+    public synchronized void fizz() throws InterruptedException {
         while (current <= n) {
-            synchronized (lock) {
-                if (current % 3 == 0 && current % 5 != 0) {
-                    System.out.println("fizz");
-                    current++;
-                    lock.notifyAll();
-                } else {
-                    lock.wait();
-                }
+            while (current % 3 != 0 || current % 5 == 0) {
+                wait();
             }
+            if (current > n) break;
+            System.out.println("fizz");
+            current++;
+            notifyAll();
         }
     }
 
-    public void buzz() throws InterruptedException {
+    public synchronized void buzz() throws InterruptedException {
         while (current <= n) {
-            synchronized (lock) {
-                if (current % 5 == 0 && current % 3 != 0) {
-                    System.out.println("buzz");
-                    current++;
-                    lock.notifyAll();
-                } else {
-                    lock.wait();
-                }
+            while (current % 5 != 0 || current % 3 == 0) {
+                wait();
             }
+            if (current > n) break;
+            System.out.println("buzz");
+            current++;
+            notifyAll();
         }
     }
 
-    public void fizzbuzz() throws InterruptedException {
+    public synchronized void fizzbuzz() throws InterruptedException {
         while (current <= n) {
-            synchronized (lock) {
-                if (current % 3 == 0 && current % 5 == 0) {
-                    System.out.println("fizzbuzz");
-                    current++;
-                    lock.notifyAll();
-                } else {
-                    lock.wait();
-                }
+            while (current % 15 != 0) {
+                wait();
             }
+            if (current > n) break;
+            System.out.println("fizzbuzz");
+            current++;
+            notifyAll();
         }
     }
 
-    public void number() throws InterruptedException {
+    public synchronized void number() throws InterruptedException {
         while (current <= n) {
-            synchronized (lock) {
-                if (current % 3 != 0 && current % 5 != 0) {
-                    System.out.println(current);
-                    current++;
-                    lock.notifyAll();
-                } else {
-                    lock.wait();
-                }
+            while (current % 3 == 0 || current % 5 == 0) {
+                wait();
             }
+            if (current > n) break;
+            System.out.println(current);
+            current++;
+            notifyAll();
         }
     }
 
